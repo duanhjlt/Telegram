bc36cb2237461bc00e053d087419181786db83af
duanhongjin <duanhjlt@163.com>
2018-03-11 17:10:22 +0800

    update

--- config.h
+++ config.h
@@ -8,8 +8,8 @@
 #ifndef config_h
 #define config_h
 
-#define SETUP_API_ID(v) v = 888;
-#define SETUP_API_HASH(v) v = @"<YOUR API HASH>";
+#define SETUP_API_ID(v) v = 149640;
+#define SETUP_API_HASH(v) v = @"7f334a2d2e959f3bddd0b583158a8389";
 
 #endif /* config_h */
 
